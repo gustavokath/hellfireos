4773f2ce026bdca887455c89210752465eb5d8b2
14104915 <14104915@facin.portoalegre.pucrsnet.br>
2018-04-25 21:12:29 -0300

    Working

--- app/aperiodic_task/aperiodic_scheduler.c
+++ app/aperiodic_task/aperiodic_scheduler.c
@@ -2,36 +2,99 @@
 
 void aperiodic_scheduler(void)
 {
-	tcb_entry* task;
+	int32_t rc;
+	volatile int32_t status;
+
+	struct tcb_entry* task;
 	for(;;){
+		
+		printf("Scheduler!\n");
 		task = hf_queue_remhead(krnl_aperiodic_queue);
 		if(!task){
+			printf("Scheduler! - No Task\n");
 			hf_yield();
 			continue;
 		}
 
 		if(task->apjobs == 0){
+			printf("Scheduler! - No Jobs for %d\n", task->id);
 			hf_kill(task->id);
 			continue;
 		}
 
+		status = _di();
 		task->apjobs--;
 		if (hf_queue_addtail(krnl_aperiodic_queue, task))
 			panic(PANIC_CANT_PLACE_APERIODIC);
 		
-		//Executa
+		printf("Scheduler! - Executing %d\n", task->id);
+		krnl_task = &krnl_tcb[krnl_current_task];
+
+		rc = _context_save(krnl_task->task_context);
+		if (rc){
+			printf("Scheduler! Start Interruption\n");
+			_ei(status);
+			continue;
+		}
+
+		if (krnl_task->state == TASK_RUNNING)
+			krnl_task->state = TASK_READY;
+		if (krnl_task->pstack[0] != STACK_MAGIC)
+			panic(PANIC_STACK_OVERFLOW);
+
+		// task->state = TASK_RUNNING;
+
+		// _context_restore(task->task_context, 1);
+		
+
 	}
 }
 
-static tcb_entry* aperiodic_queue_next()
+void task1(void)
 {
-	return 
+	int32_t jobs, id;
+	id = hf_selfid();
+	for(;;){
+		jobs = hf_jobs(id);
+		printf("Task Melhor esforco\n");
+		//printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
+}
+
+void task2(void)
+{
+	int32_t jobs, id;
+	id = hf_selfid();
+	for(;;){
+		jobs = hf_jobs(id);
+		printf("Task Aperiodica\n");
+		//printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
+}
+
+void task3(void)
+{
+	int32_t jobs, id;
+	id = hf_selfid();
+	for(;;){
+		jobs = hf_jobs(id);
+		printf("Task Periodica\n");
+		//printf("\n%s (%d)[%d][%d]", hf_selfname(), id, hf_jobs(id), hf_dlm(id));
+		while (jobs == hf_jobs(id));
+	}
 }
 
 void app_main(void){
 	int32_t i;
 	
-	for (i = 0; i < 20; i++)
-		hf_spawn(thread, 0, 0, 0, "thread", 1024);
+	
+	//hf_spawn(task1, 0, 0, 0, "task1", 1024);
+	hf_spawn(task2, 0, 3, 0, "task2", 1024);
+	//hf_spawn(task3, 3, 1, 2, "task3", 1024);
+	hf_spawn(aperiodic_scheduler, 10, 1, 10, "aperiodic_scheduler", 1024);
+	
+	
 }
 
--- sys/kernel/task.c
+++ sys/kernel/task.c
@@ -229,8 +229,8 @@ int32_t hf_spawn(void (*task)(), uint16_t period, uint16_t capacity, uint16_t de
 #if KERNEL_LOG == 2
 	dprintf("hf_spawn() %d ", (uint32_t)_read_us());
 #endif
-	if ((period < capacity) || (deadline < capacity))
-		return ERR_INVALID_PARAMETER;
+
+	kprintf("\n\n#####%s\n", name);
 	
 	status = _di();
 	while ((krnl_tcb[i].ptask != 0) && (i < MAX_TASKS))
@@ -255,7 +255,7 @@ int32_t hf_spawn(void (*task)(), uint16_t period, uint16_t capacity, uint16_t de
 	krnl_task->deadline_rem = deadline;
 	krnl_task->rtjobs = 0;
 	krnl_task->bgjobs = 0;
-	krnl_task->apjobs = 0;	
+	krnl_task->apjobs = capacity;	
 	krnl_task->deadline_misses = 0;
 	krnl_task->ptask = task;
 	stack_size += 3;
@@ -269,10 +269,13 @@ int32_t hf_spawn(void (*task)(), uint16_t period, uint16_t capacity, uint16_t de
 		krnl_task->pstack[0] = STACK_MAGIC;
 		kprintf("\nKERNEL: [%s], id: %d, p:%d, c:%d, d:%d, addr: %x, sp: %x, ss: %d bytes", krnl_task->name, krnl_task->id, krnl_task->period, krnl_task->capacity, krnl_task->deadline, krnl_task->ptask, _get_task_sp(krnl_task->id), stack_size);
 		if (period && capacity && deadline){ // Check for real time tasks
+			kprintf("[TASK] Periodic!\n");
 			if (hf_queue_addtail(krnl_rt_queue, krnl_task)) panic(PANIC_CANT_PLACE_RT);
 		}else if(!period && !capacity && !deadline){ // Check for best effort
+			kprintf("[TASK] Melhor esforco!\n");
 			if (hf_queue_addtail(krnl_run_queue, krnl_task)) panic(PANIC_CANT_PLACE_RUN);
 		}else if(!period && !deadline && capacity){ //Check for aperiodic tasks
+			kprintf("[TASK] Aperiodic!\n");
 			if (hf_queue_addtail(krnl_aperiodic_queue, krnl_task)) panic(PANIC_CANT_PLACE_APERIODIC);
 		}
 	}else{
